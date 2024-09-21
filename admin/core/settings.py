48a928271205c91ae18b76bd1685e00bf32d8dce
dosxana <dosxana@gmail.com>
2024-09-21 18:11:10 +0500

    Исправление конфигов nginx для выдачи статики

--- admin/core/settings.py
+++ admin/core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wxmj@9v*3#*^gtt=6ypajq(wcayorkz-vf_i7&(xy$dbg#%rr&'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG')))
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
